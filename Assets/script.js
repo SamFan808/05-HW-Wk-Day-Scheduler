3918a34492d2821bd9a44f212c6be91f0346131d
Sam Fan <samisfan@msn.com>
2020-10-21 14:22:28 -0600

    started pseudo code

---
+++ Assets/script.js
@@ -0,0 +1,9 @@
+//pseudo code time
+// calendar
+// Create input field, save button
+// js controls styling based on time of day, css changes color of input field backgrounds, queries moment.js for current time?
+// Date display at top of screen (third line) centered, and queries moment.js for current date
+// time blocks .textContent to left of input field, static
+// Event listener for clicks, 
+// user inputs information into input field - the save button saves to local storage
+// 
