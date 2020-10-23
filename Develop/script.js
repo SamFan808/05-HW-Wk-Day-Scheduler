4854ce3a8b5e1b2d7662fec971c9c21be337541a
Sam Fan <samisfan@msn.com>
2020-10-23 13:03:12 -0600

    solved some nodelist DOM targetting issues, buttons work as expected, sorting out localStorage, if else current time next

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,8 @@
-//pseudo code time
-// calendar
-// Create input field, save buttons
-// append input field and save buttons, timeofDay[i] times
-// js controls styling based on time of day, css changes color of input field backgrounds, queries moment.js for current time?
-// Date display at top of screen (third line) centered, and queries moment.js for current date
-// time blocks .textContent to left of input field, static
-// Event listener for clicks, 
-// user inputs information into input field - the save button saves to local storage
-// for loop to iterate through times of day in left hand field, or get times from moment.js?
-// each line of input has timeofDay[i]   input field    save button
-
-
 // Variables
 var dateCurrent = document.getElementById("currentDay");
 var containerC1 = document.querySelector(".container");
-var btn = document.querySelector(".saveBtn");
-var inputBlock = document.querySelectorAll("textarea");
+var btn = document.getElementsByTagName("button");
+var inputBlock = document.getElementsByTagName("textarea");
 // array to store hourly items
 var inputItems = [];
 // momentjs queries
@@ -26,21 +13,23 @@ dateCurrent.textContent = timeTest.format("dddd, MMMM Do");
 function inputStore () {
     localStorage.setItem("inputItems", JSON.stringify(inputItems));   
 }
-// clicking save buttons adds to localStorage
+// for loop to iterate through nodelist of buttons and textareas, added event listener to allow save button to store data in localStorage 
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", function (event) {
+        event.preventDefault();
+        this.setAttribute("data-index", i);
 
-btn.addEventListener("click", function (event) {
-    event.preventDefault();
-    var inputText = inputBlock.value.trim();
-    // if save is clicked without any input, nothing is added
-    if (inputText === "") {
+        var inputText = inputBlock[i].value.trim();
+        // if save is clicked without any input, nothing is added
+        if (inputText === "") {
         return;
-    }
-    // adds the the inputItems array to be store in localStorage
-    inputItems.push(inputText);
-    inputBlock.value = "";
-    // store update items to localStorage
-    inputStore();
-});
+        }
+        // adds the the inputItems array to be store in localStorage
+        inputItems.push(inputText);
+        // store update items to localStorage
+        inputStore();
+    });
+}
 // checks localStorage to see if ithere is any items stored and if they are, assign to the inputDaily
 function init () {
     var stored = JSON.parse(localStorage.getItem("inputItems"));
