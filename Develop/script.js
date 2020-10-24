af8592651afb1e473a7045940525f4b6af362595
Sam Fan <samisfan@msn.com>
2020-10-23 23:10:59 -0600

    added some moment functions, working on the color indicator

--- Develop/script.js
+++ Develop/script.js
@@ -7,26 +7,28 @@ var inputBlock = document.getElementsByTagName("textarea");
 var inputItems = [];
 // momentjs queries
 var timeTest = moment();
-var timeNow = moment().format('LT');
+var timeNow = moment();
 dateCurrent.textContent = timeTest.format("dddd, MMMM Do");
+
 // function stores input to local storage
 function inputStore () {
     localStorage.setItem("inputItems", JSON.stringify(inputItems));   
 }
 // for loop sets the input block attributes for each textarea
 for (let i = 0; i < inputBlock.length; i++) {
-        inputBlock[i].setAttribute("data-index", i);
 }
+// sets click event for each button and stores text from textarea into inputItems array
 
 for (let i = 0; i < inputBlock.length; i++) {
     btn[i].addEventListener("click", function (event) {
         event.preventDefault();
         // if save is clicked without any input, nothing is added
         var inputText = inputBlock[i].value.trim();
+        inputBlock[i].setAttribute("data-index", i);
         if (inputText === "") {
-        return;
+            return;
         } else {
-        // adds the the inputItems array to be store in localStorage
+        // adds the the inputItems array to be stored in localStorage
         inputItems.push(inputText);
         // store update items to localStorage
         inputStore();
@@ -34,9 +36,9 @@ for (let i = 0; i < inputBlock.length; i++) {
         }
     });
 }
-var stored = JSON.parse(localStorage.getItem("inputItems"));
 // checks localStorage to see if there is any items stored and if they are, assign to the inputItems
 function init () {
+    var stored = JSON.parse(localStorage.getItem("inputItems"));
     if (stored !== null) {
     inputItems = stored;
     }
@@ -44,23 +46,28 @@ function init () {
 }
 // function to clear the page daily before it refreshes at 9AM
 function clear () {
-    var opening = moment('9:00 AM', 'LT');
-    var closing = moment('5:00 PM', 'LT');
-    console.log(opening);
-    // if (timeNow.isAfter(opening) && currentTime.isBefore(closing)){
-    console.log("It's business time!")
+    var format = 'hh:mm:ss';
+    var beforeTime = moment('23:59:00', format);
+    var afterTime = moment('08:59:00', format);
+    if (timeNow.isBetween(beforeTime, afterTime)) {
+        console.log('is between')
+    } else {
+        console.log('is not between')
     }
-  
-    // if ("8:59 AM" > timeNow > "11:59 PM" ) {
-    //     // inputItems = ""
-    //     console.log("something is working")
-    // //something local time
-    // } else {
-    // console.log("poopty poopty pants");
-    // }
-
-
+}
+var hourData = document.getElementById("9am");
+function colorTime () {
+    if (hourData===moment().format("h")) {
+        console.log("hit");
+    } else {
+        console.log("no hit");
+    }
+}
+console.log(moment().format("ha"));
+colorTime();
 init();
+clear();
+inputStore();
 
 // function renderList () {
 //     for (var i = 0; i < inputItems.length; i++) {
@@ -68,10 +75,9 @@ init();
 //         inputBlock[i].textContent = inputs;
 //         inputItems.splice("data-index", 2);
 //     }
-// }
+// 
+
 // each input from textarea needs a unique identifier, each textarea needs a unique identifier (index#?), getItem needs to be placed back in each textarea 
 
 // for loop to iterate through nodelist of buttons and textareas, added event listener to allow save button to store data in localStorage
-clear();
-inputStore();
-init();
+
