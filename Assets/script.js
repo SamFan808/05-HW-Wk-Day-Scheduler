05fd075eb68e9ea5f81a5c2bdea3aa6bff56758a
Sam Fan <samisfan@msn.com>
2020-10-25 10:52:37 -0600

    small tweaks, cleaning up code

--- Assets/script.js
+++ Assets/script.js
@@ -3,17 +3,16 @@ var containerC1 = document.querySelector(".container");
 var btn = document.getElementsByTagName("button");
 var inputBlock = document.getElementsByTagName("textarea");
 var hourData = document.getElementsByClassName("hour");
-var storeArray = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+var storeArray = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 // momentjs queries
-var timeTest = moment();
 var timeNow = moment();
-dateCurrent.textContent = timeTest.format("dddd, MMMM Do");
+dateCurrent.textContent = timeNow.format("dddd, MMMM Do");
 // sets click event for each button and stores items into localStorage
 for (let i = 0; i < inputBlock.length; i++) {
     btn[i].addEventListener("click", function (event) {
         event.preventDefault();
         setStore();
-   });
+    });
 }
 function setStore () {
     for (i = 0; i < storeArray.length; i++) {
@@ -22,22 +21,22 @@ function setStore () {
 }
 function render () {
     for (i = 0; i < storeArray.length; i++) {
-    document.getElementById(storeArray[i]).innerHTML+=(localStorage.getItem(storeArray[i]));
+        document.getElementById(storeArray[i]).innerHTML+=(localStorage.getItem(storeArray[i]));
     }
 }
 // function to clear the page daily before it refreshes at 9AM
 function clear () {
     var format = 'hh:mm:ss';
-    var beforeTime = moment('23:59:00', format);
-    var afterTime = moment('08:59:00', format);
+    var beforeTime = moment("23:59:00", "HH:mm");
+    var afterTime = moment("08:59:00", "HH:mm");
     if (timeNow.isBetween(beforeTime, afterTime)) {
         for (i = 0; i < storeArray.length; i++) {
             localStorage.setItem(storeArray[i],"");
         }
-        } else {
+    }   else {
             console.log("is not between");
         }
-    }
+}
 // function sets current, future, and past color indicators
 function colorTime () {
     for (let i = 0; i < hourData.length; i++) {
